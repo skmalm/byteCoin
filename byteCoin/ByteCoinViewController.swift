3d36483b62af0e5fac80bfffe2405828f8489a28
Sebastian Malm <skmalm@gmail.com>
2020-03-01 08:28:14 -0500

    feat: Initial setup: make launch screen, organize files

---
+++ byteCoin/ByteCoinViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  byteCoin
+//
+//  Created by Sebastian Malm on 3/1/20.
+//  Copyright © 2020 SebastianMalm. All rights reserved.
+//
+
+import UIKit
+
+class ByteCoinViewController: UIViewController {
+    
+}
+
